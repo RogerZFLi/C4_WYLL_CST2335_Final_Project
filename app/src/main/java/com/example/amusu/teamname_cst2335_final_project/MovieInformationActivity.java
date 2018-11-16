96f4535bb4c4aba777a65e047f502ba5ddac8315
Charlie <Cr841203>
2018-11-16 14:48:49 -0500

    food nutrition part

--- app/src/main/java/com/example/amusu/teamname_cst2335_final_project/MovieInformationActivity.java
+++ app/src/main/java/com/example/amusu/teamname_cst2335_final_project/MovieInformationActivity.java
@@ -3,6 +3,7 @@ package com.example.amusu.teamname_cst2335_final_project;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
